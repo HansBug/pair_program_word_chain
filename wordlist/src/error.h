7dcd28b3400888a4e5643fcd3da51fc0d7a5c7da
fondoger <fondoger@outlook.com>
2019-03-08 23:31:48 +0800

    add funcitons for reading words from file

--- wordlist/src/error.h
+++ wordlist/src/error.h
@@ -2,7 +2,11 @@
 #define ERROR_H_
 
 
-#define ERR_FAILED_OPEN_FILE "failed open file"
+#define ERR_FAILED_OPEN_FILE        "failed open file"
+#define ERR_MISSING_OPTION_W_C      "missing option -w or -c"
+#define ERR_WRONG_FORMAT_OPTION_H   "wrong format of option -h"
+#define ERR_WRONG_FORMAT_OPTION_T   "wrong format of option -t"
+#define ERR_NO_VALID_WORDS          "no valid english words"
 
 void error(const char *msg);
 
--- wordlist/src/main.cpp
+++ wordlist/src/main.cpp
@@ -2,45 +2,112 @@
 #include <stdio.h>
 #include <fstream>
 #include <cassert>
+#include <unordered_set>
+#include <cctype>
 #include "../lib/getopt_pp/getopt_pp.h"
 #include "error.h"
 // #include "../../core/core.h"
 // #pragma comment(lib, "C:/Desktop/pair_program_word_chain/core/build/libcore")
 
+typedef struct _Params {
+    bool flag_w, flag_c;
+    char flag_h, flag_t;
+    bool flag_r;
+} Params;
 
 
-int main(int argc, char *argv[])
+Params parseArguments(GetOpt::GetOpt_pp &ops) 
 {
-    bool flag_w, flag_c, flag_h, flag_t, flag_r;
-    std::string filename = "../input.txt";
+    bool flag_w, flag_c;
+    char flag_h, flag_t;
+    bool flag_r;
 
-    // Step 1: parse parameters
-    GetOpt::GetOpt_pp ops(argc, argv);
+    //GetOpt::GetOpt_pp ops(argc, argv);
     ops >> GetOpt::OptionPresent('w', flag_w);
     ops >> GetOpt::OptionPresent('c', flag_c);
-    ops >> GetOpt::OptionPresent('h', flag_h);
-    ops >> GetOpt::OptionPresent('t', flag_t);
-    ops >> GetOpt::OptionPresent('r', flag_r);
-    if (!(ops >> GetOpt::GlobalOption(filename))) {
-        std::cout << "using default file: " << filename << std::endl;
+    if (ops >> GetOpt::OptionPresent('h')) {
+        if (!(ops >> GetOpt::Option('h', flag_h))) {
+            error(ERR_WRONG_FORMAT_OPTION_H);
+        }
     }
+    if (ops >> GetOpt::OptionPresent('t')) {
+        if (!(ops >> GetOpt::Option('t', flag_t))) {
+            error(ERR_WRONG_FORMAT_OPTION_T);
+        }
+    }
+    ops >> GetOpt::OptionPresent('r', flag_r);
     /*
     printf("w:%d, c:%d, h:%d, t:%d, r:%d\n", 
         flag_w, flag_c, flag_h, flag_t, flag_r);
     printf("filename: %s\n", filename.c_str());
     */
+   return Params{flag_w, flag_c, flag_h, flag_t, flag_r};
+}
 
 
+/**
+ * Read english words from an input stream
+ * 
+ * Result is stored into `words`
+ */
+void readWords(std::vector<std::string> &words, std::istream &input_stream)
+{
+    char ch;
+    bool empty = true;
+    std::stringstream word;   
+    std::unordered_set<std::string> words_set;
+
+    /**
+     * TODO: 
+     * which is faster ?
+     * stringstream's << operator and string's + operter
+     */
+
+    // read char by char
+    while (input_stream >> std::noskipws >> ch) {
+        if (std::isalpha(ch)) {
+            assert(ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z');
+            word << (char)std::tolower(ch);
+            empty = false;
+        } else if (!empty) {
+            words_set.insert(word.str());
+            word.str("");
+            empty = true;
+        }
+    }
+
+    if (words_set.empty()) {
+        error(ERR_NO_VALID_WORDS);
+    }
+
+    words.insert(words.end(), words_set.begin(), words_set.end());
+}
+
+
+int main(int argc, char *argv[])
+{
+    std::string filename = "../input.txt";
+    std::vector<std::string> words;
+    GetOpt::GetOpt_pp ops(argc, argv);
+
+    if (!(ops >> GetOpt::GlobalOption(filename))) {
+        std::cout << "Not providing input file, ";
+        std::cout << "using default file: " << filename << std::endl;
+    }
 
     // Step 2: read file char by char
     char ch;
-    std::fstream fin(filename, std::fstream::in);
+    std::ifstream fin(filename, std::fstream::in);
     if (!fin.is_open()) {
         std::cout << "filename: " << filename << std::endl;
         error(ERR_FAILED_OPEN_FILE);
     }
-    while (fin >> std::noskipws >> ch) {
-        printf("%c", ch);
+    readWords(words, fin);
+    fin.close();
+
+    Params params = parseArguments(ops);
+    for (std::string word : words) {
+        std::cout << word << " " << std::endl;
     }
 
     return 0;    
